ce05d1c0aa98ef8b9b6433c89aacdfb2ead88481
crabvendor <st.wyrobek@gmail.com>
2019-10-10 00:51:19 +0200

    move config variables to config module

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 
-// import { roleAssigner } from './roleAssignment.js';
 const Discord = require('discord.js');
+const { channelID, messageID, TOKEN } = require('./config.js');
 const RoleAssignment = require('./roleAssignment.js');
 const roleAssigner = new RoleAssignment.RoleAssigner();
 const bot = new Discord.Client();
 
-// Insert your authentication token:
-const TOKEN = "authToken";
-
-
 const cacheReactionMessage = async () => {
-    // Insert id of message (and it's channel) that's listening to reactions:
-    const channelId = "channelId";
-    const messageId = "messageId";
-    const reactionChannel = bot.channels.get(channelId);
+    const reactionChannel = bot.channels.get(channelID);
     try {
         await reactionChannel.fetchMessage(messageId);     
         await console.log(`Fetched and cached message: ${messageId}`);
@@ -29,5 +22,4 @@ bot.on('ready', () => {
 
 bot.on('messageReactionAdd', roleAssigner.grantRole);    
 bot.on('messageReactionRemove', roleAssigner.removeRole);
-
 bot.login(TOKEN);
--- roles.js
+++ roles.js
@@ -1,4 +1,4 @@
 // Insert name of emojis that trigger reactions mapped to name of correspondive roles:
 module.exports  = {
     "SabaPing": "Fish"
-}
+}   
