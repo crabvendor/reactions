b15385666d7d1d2f24cd609be515d6ab74b6bdd0
crabvendor <st.wyrobek@gmail.com>
2020-02-01 11:27:20 +0100

    Add default config file

---
+++ config.js
@@ -0,0 +1,6 @@
+// set authorization token
+exports.TOKEN = "TOKEN";
+
+// set channel and message which is listening to reactions
+exports.channelID = "channelId";
+exports.messageID = "messageID";
