57645ca711114928ecd98ea2dbd0084171829f53
crabvendor <st.wyrobek@gmail.com>
2021-02-04 13:59:25 +0100

    replace config values

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // set authorization token
-exports.TOKEN = "NjMxNTIyNDgyMDU4Mjk3Mzc1.XZ4FJA.VezoKu9UqfdgryFU1Nx1W8UuR9g";
+exports.TOKEN = "TOKEN";
 
 // set channel and message which is listening to reactions
-exports.messageID = "631822202815381505";
+exports.messageID = "MESSAGE";
